548e8bb215bbf61922a4e70257c40dbf9bed90d8
edman-cota <edmancota2017@gmail.com>
2022-11-07 17:38:33 -0600

    feat(installation): add Framework Guide section skeleton

--- pages/docs/installation.tsx
+++ pages/docs/installation.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlockCodeUsage from '../../components/blockcodes/Usage'
 import BlockCodeInstallation from '../../components/blockcodes/Installation'
+import Link from 'next/link'
 
 const installation = () => {
   return (
@@ -15,7 +16,7 @@ const installation = () => {
           gives you ready-to-use React components.
         </p>
       </header>
-      <section className='mb-16 relative'>
+      <section className='relative'>
         <div>
           <h2 className='text-slate-200 text-xl tracking-tight font-bold mb-3'>
             Installation
@@ -42,11 +43,111 @@ const installation = () => {
           </p>
         </div>
         <BlockCodeUsage />
+      </section>
+      <section className='relative'>
         <div>
           <h2 className='text-slate-200 text-xl tracking-tight font-bold mb-3 mt-10'>
-            Demo
+            Framework Guide
           </h2>
         </div>
+        <div className='relative z-10 max-w-3xl mb-6'>
+          <p className='text-slate-400'>
+            Framework-specific guides that cover our recommended approach to
+            installing Serity UI.
+          </p>
+        </div>
+        <ul className='grid grid-cols-1 gap-x-6 gap-y-10 mt-8 sm:grid-cols-2 xl:grid-cols-3'>
+          <li className='relative flex flex-row-reverse'>
+            <div className='peer group ml-6 flex-auto'>
+              <h4 className='mb-2 leading-6 text-slate-200 font-semibold'>
+                <Link
+                  className='before:absolute before:-inset-3 before:rounded-2xl'
+                  href='/docs/guides/nextjs'
+                >
+                  Next.js
+                  <svg
+                    viewBox='0 0 3 6'
+                    className='ml-3 w-auto h-1.5 overflow-visible inline -mt-px text-slate-400 opacity-0 group-hover:opacity-100 group-focus-within:opacity-100'
+                  >
+                    <path
+                      d='M0 0L3 3L0 6'
+                      fill='none'
+                      stroke-width='2'
+                      stroke='currentColor'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    ></path>
+                  </svg>
+                </Link>
+              </h4>
+              <p className='text-sm leading-6 text-slate-400'>
+                Full-featured React framework wihth great developer experience.
+              </p>
+            </div>
+            <div className='flex-none w-14 h-14 rounded-full ring-1 ring-slate-900/5 shadow flex-items-center justify-center overflow-hidden bg-slate-800 highlight-white/5'></div>
+            <div className='absolute -z-10 -inset-3 rounded-2xl bg-slate-800/50 opacity-0 peer-hover:opacity-100'></div>
+          </li>
+          <li className='relative flex flex-row-reverse'>
+            <div className='peer group ml-6 flex-auto'>
+              <h4 className='mb-2 leading-6 text-slate-200 font-semibold'>
+                <Link
+                  className='before:absolute before:-inset-3 before:rounded-2xl'
+                  href='/docs/guides/nextjs'
+                >
+                  Vite
+                  <svg
+                    viewBox='0 0 3 6'
+                    className='ml-3 w-auto h-1.5 overflow-visible inline -mt-px text-slate-400 opacity-0 group-hover:opacity-100 group-focus-within:opacity-100'
+                  >
+                    <path
+                      d='M0 0L3 3L0 6'
+                      fill='none'
+                      stroke-width='2'
+                      stroke='currentColor'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    ></path>
+                  </svg>
+                </Link>
+              </h4>
+              <p className='text-sm leading-6 text-slate-400'>
+                Fast and modern development server and build tool.
+              </p>
+            </div>
+            <div className='flex-none w-14 h-14 rounded-full ring-1 ring-slate-900/5 shadow flex-items-center justify-center overflow-hidden bg-slate-800 highlight-white/5'></div>
+            <div className='absolute -z-10 -inset-3 rounded-2xl bg-slate-800/50 opacity-0 peer-hover:opacity-100'></div>
+          </li>
+          <li className='relative flex flex-row-reverse'>
+            <div className='peer group ml-6 flex-auto'>
+              <h4 className='mb-2 leading-6 text-slate-200 font-semibold'>
+                <Link
+                  className='before:absolute before:-inset-3 before:rounded-2xl'
+                  href='/docs/guides/nextjs'
+                >
+                  Create React App
+                  <svg
+                    viewBox='0 0 3 6'
+                    className='ml-3 w-auto h-1.5 overflow-visible inline -mt-px text-slate-400 opacity-0 group-hover:opacity-100 group-focus-within:opacity-100'
+                  >
+                    <path
+                      d='M0 0L3 3L0 6'
+                      fill='none'
+                      stroke-width='2'
+                      stroke='currentColor'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    ></path>
+                  </svg>
+                </Link>
+              </h4>
+              <p className='text-sm leading-6 text-slate-400'>
+                CLI tool for scaffolding a new single-page React application.
+              </p>
+            </div>
+            <div className='flex-none w-14 h-14 rounded-full ring-1 ring-slate-900/5 shadow flex-items-center justify-center overflow-hidden bg-slate-800 highlight-white/5'></div>
+            <div className='absolute -z-10 -inset-3 rounded-2xl bg-slate-800/50 opacity-0 peer-hover:opacity-100'></div>
+          </li>
+        </ul>
       </section>
     </Layout>
   )
