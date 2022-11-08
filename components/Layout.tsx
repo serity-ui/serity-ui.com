dcfcddbba3df52a1f5f726f33b23f40fe8a2abd0
edman-cota <edmancota2017@gmail.com>
2022-11-07 19:08:01 -0600

    feat(installation): add footer section and reorganize sidebar items

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Sidebar from './Sidebar'
 
 const Layout = ({ children }: any) => {
@@ -9,6 +10,19 @@ const Layout = ({ children }: any) => {
           <main className='max-w-3xl mx-auto relative z-20 flex flex-col overflow-hidden px-12 py-10  xl:max-w-none text-white'>
             {children}
           </main>
+          <footer className='text-sm leading-6 mt-16'>
+            <div className='pt-10 pb-28 border-t sm:flex justify-between text-slate-500 border-slate-200/5'>
+              <div className='mb-6 sm:mb-0 sm:flex'>
+                <p>Copyright © 2022 Serity Inc.</p>
+                <p className='sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200/5'>
+                  <Link className='hover:text-slate-400' href='/brand'>
+                    Trademark Policy
+                  </Link>
+                </p>
+              </div>
+              <div></div>
+            </div>
+          </footer>
         </div>
       </div>
     </div>
